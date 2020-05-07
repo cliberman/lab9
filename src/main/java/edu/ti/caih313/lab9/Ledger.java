850e3a47980a3a5dd0a9df6a6851775fad4ba5cf
cliberman <chanaliberman1@gmail.com>
2020-05-07 12:59:24 -0500

    new version-all works

--- src/main/java/edu/ti/caih313/lab9/Ledger.java
+++ src/main/java/edu/ti/caih313/lab9/Ledger.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 
 public class Ledger {
 
-    private static Double numberOfSales = 0.0;
-    private static Double totalSalesValue = 0.0;
-    private static Double averageSaleValue = 0.0;
-    private static Double numberOfHighValueSales = 0.0;
-    private static ArrayList<Double> salesList = new ArrayList<Double>();
+    private Double numberOfSales = 0.0;
+    private Double totalSalesValue = 0.0;
+    private Double averageSaleValue = 0.0;
+    private Double numberOfHighValueSales = 0.0;
+    private ArrayList<Double> salesList = new ArrayList<Double>();
 
-    public static void addSale(double d) { //addSale(d)—adds a sale whose value is d
+    public void addSale(double d) { //addSale(d)—adds a sale whose value is d
         numberOfSales++;
         totalSalesValue += d;
         salesList.add(d);
     }
-    public static Double getNumberOfSales() { //getNumberOfSales—returns the number of sales made
+    public Double getNumberOfSales() { //getNumberOfSales—returns the number of sales made
         return numberOfSales;
     }
-    public static Double getTotalSales() { //getTotalSales—returns the total value of the sales
+    public Double getTotalSales() { //getTotalSales—returns the total value of the sales
         return totalSalesValue;
     }
-    public static Double getAverageSale() { //getAverageSale()—returns the average value of all the sales
+    public Double getAverageSale() { //getAverageSale()—returns the average value of all the sales
         averageSaleValue = totalSalesValue/numberOfSales;
         return averageSaleValue;
     }
-    public static Double getCountAbove(double v) { //getCountAbove(v)—returns the number of sales that exceeded v in value
+    public Double getCountAbove(double v) { //getCountAbove(v)—returns the number of sales that exceeded v in value
         for(int i=0;i<salesList.size();i++) {
             if (salesList.get(i) > v) {
                 numberOfHighValueSales++;
@@ -36,6 +36,7 @@ public class Ledger {
     }
 
     public static void main(String[] args) {
+        Ledger ledger = new Ledger();
         boolean isPositive = true;
         Scanner keyboard = new Scanner(System.in);
         Double highestSale = 0.0;
@@ -45,15 +46,15 @@ public class Ledger {
             if (saleValue < 0) {
                 isPositive = false;
             } else {
-                Ledger.addSale(saleValue);
+                ledger.addSale(saleValue);
             }
         } while (isPositive);
         System.out.println("Enter high value to see how many sales exceeded the value: ");
         Double v = keyboard.nextDouble();
-        System.out.println("Number of sales made: " + getNumberOfSales());
-        System.out.println("Average sale value: " + getAverageSale());
-        System.out.println("Total value of sales: " + getTotalSales());
-        System.out.println("Number of sales over the value of  " + v + ": " + getCountAbove(v));
+        System.out.println("Number of sales made: " + ledger.getNumberOfSales());
+        System.out.println("Average sale value: " + ledger.getAverageSale());
+        System.out.println("Total value of sales: " + ledger.getTotalSales());
+        System.out.println("Number of sales over the value of  " + v + ": " + ledger.getCountAbove(v));
     }
 }
 
